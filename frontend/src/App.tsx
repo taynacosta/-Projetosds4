9d8496a07b54a530f6adee5e7c414a8052a1dd5b
taynacosta <taynamoreira1811@gmail.com>
2021-09-11 07:08:17 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
+import Routes from "Routes";
+
 function App() {
   return (
-    <div>
-      <h1>Olá mundo!</h1>
-    </div>
+    <Routes />
   );
 }
 
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+    return (
+        <div>
+            <footer className="footer mt-auto py-3 bg-dark">
+                <div className="container">
+                    <p className="text-light">App desenvolvido por <a href="https:/github.com/taynacosta" target="_blank" rel="noreferrer">Tayná Costa</a></p>
+                    <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                        Evento promovido pela escola DevSuperior: <a href="https:/instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+                </div>
+            </footer>
+
+        </div>
+    );
+}
+
+export default Footer;
